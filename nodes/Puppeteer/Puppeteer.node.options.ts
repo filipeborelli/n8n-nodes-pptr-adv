1ef175447098eab00707a9e815e5e27fe0ecff93
Filipe Borelli <filipeborelli1@hotmail.com>
2024-10-21 14:40:18 -0300

    Changes

--- nodes/Puppeteer/Puppeteer.node.options.ts
+++ nodes/Puppeteer/Puppeteer.node.options.ts
@@ -23,55 +23,6 @@ export const nodeDescription: INodeTypeDescription = {
 			type: "options",
 			options: [
 				{
-					name: "Browser",
-					value: "browser",
-					description: "Browser context",
-				},
-				{
-					name: "Page",
-					value: "page",
-					description: "page context",
-				},
-				{
-					name: "Target",
-					value: "target",
-					description: "target context",
-				},
-				{
-					name: "Mouse",
-					value: "mouse",
-					description: "mouse context",
-				},
-				{
-					name: "Keyboard",
-					value: "keyboard",
-					description: "keyboard context",
-				},
-				{
-					name: "Frame",
-					value: "frame",
-					description: "frame context",
-				},
-				{
-					name: "Dialog",
-					value: "dialog",
-					description: "dialog context",
-				},
-				{
-					name: "Network",
-					value: "network",
-					description: "network context",
-				},
-			],
-			default: "browser",
-		},
-		{
-			displayName: "Browser Operation",
-			name: "browserOperation",
-			type: "options",
-			required: true,
-			options: [
-				{
 					name: "Launch",
 					value: "launchBrowser",
 					description: "Launch a new browser instance",
@@ -81,21 +32,97 @@ export const nodeDescription: INodeTypeDescription = {
 					value: "connectBrowser",
 					description: "Connect to an existing browser instance",
 				},
-				{
-					name: "Close",
-					value: "closeBrowser",
-					description: "Close the browser instance",
-				},
-				{
-					name: "Test",
-					value: "testBrowser",
-					description: "Test the browser instance",
-				},
 			],
 			default: "launchBrowser",
+		},
+		{
+			displayName: "Browser WSEndpoint",
+			name: "browserWSEndpoint",
+			type: "string",
+			required: true,
+			default: "",
 			displayOptions: {
 				show: {
-					operation: ["browser"],
+					operation: ["connectBrowser"],
+				},
+			},
+		},
+		{
+			displayName: "Browser Options",
+			name: "browserOptions",
+			type: "collection",
+			placeholder: "Add Option",
+			default: {},
+			options: [
+				{
+					displayName: "Emulate Device",
+					name: "device",
+					type: "options",
+					default: "",
+					typeOptions: {
+						loadOptionsMethod: "getDevices",
+					},
+					required: false,
+				},
+				{
+					displayName: "Launch Arguments",
+					name: "launchArguments",
+					placeholder: "Add Argument",
+					type: "fixedCollection",
+					typeOptions: {
+						multipleValues: true,
+					},
+					description:
+						"Additional command line arguments to pass to the browser instance.",
+					default: {},
+					options: [
+						{
+							name: "args",
+							displayName: "",
+							values: [
+								{
+									displayName: "Argument",
+									name: "arg",
+									type: "string",
+									default: "",
+									description:
+										"The command line argument to pass to the browser instance.",
+								},
+							],
+						},
+					],
+				},
+				{
+					displayName: "Headless mode",
+					name: "headless",
+					type: "boolean",
+					required: false,
+					default: true,
+					description:
+						"Whether to run browser in headless mode. Defaults to true.",
+				},
+				{
+					displayName: "Stealth mode",
+					name: "stealth",
+					type: "boolean",
+					required: false,
+					default: false,
+					description:
+						"When enabled, applies various techniques to make detection of headless Puppeteer harder.",
+				},
+				{
+					displayName: "Proxy Server",
+					name: "proxyServer",
+					type: "string",
+					required: false,
+					default: "",
+					description:
+						"This tells Puppeteer to use a custom proxy configuration. Examples: localhost:8080, socks5://localhost:1080, etc.",
+				},
+			],
+			displayOptions: {
+				show: {
+					operation: ["connectBrowser", "launchBrowser"],
 				},
 			},
 		},
