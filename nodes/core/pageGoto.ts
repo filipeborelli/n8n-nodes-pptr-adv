90f70c286cbe7b18c0dce4c597dbf787abaa0e1f
Filipe Borelli <filipeborelli1@hotmail.com>
2024-10-23 23:16:09 -0300

    changes

--- nodes/core/pageGoto.ts
+++ nodes/core/pageGoto.ts
@@ -2,8 +2,8 @@ import { IPageGoto } from "./dto/interface";
 import state from "../Puppeteer/utils/Cache";
 
 export const pageGoto = async (data: IPageGoto) => {
-        const { instance, options } = data;
-        await state[instance].page.goto(options.url);
+        const { instance, options, url } = data;
+        await state[instance].page.goto(url,options);
         return {
                 page: state[instance].page,
                 browser: state[instance].browser
