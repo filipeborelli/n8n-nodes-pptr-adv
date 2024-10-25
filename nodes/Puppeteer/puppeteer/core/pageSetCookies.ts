178a15cd3a5deac9e6b4d8bf1062986d3bbcbfab
Filipe Borelli <filipeborelli1@hotmail.com>
2024-10-25 16:40:20 -0300

    Changes

--- nodes/Puppeteer/puppeteer/core/pageSetCookies.ts
+++ nodes/Puppeteer/puppeteer/core/pageSetCookies.ts
@@ -4,12 +4,13 @@ import { state } from "../state";
 export const pageSetCookies = async (data: IPageSetCookies) => {
     const { instance, cookies } = data;
     try {
-        console.log(cookies,"meus cookies")
+        console.log(cookies,"meus cookies",cookies[0]?.name,...cookies)
         const response = await state[instance]?.page.setCookie(...cookies);
         return {
             status: "success",
             message: "Cookies successfully set",
-            cookies: response
+            cookies: response,
+            data: cookies[0]?.name
         }
     } catch (error: any) {
         return {
