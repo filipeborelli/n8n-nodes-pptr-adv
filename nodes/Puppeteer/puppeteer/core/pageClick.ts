d44f5ac98dff367574752f75e4686086e2236f1d
arose26 <axelrose20262026@gmail.com>
2024-10-28 18:21:38 +0000

    Combined new features with iframe selection

--- nodes/Puppeteer/puppeteer/core/pageClick.ts
+++ nodes/Puppeteer/puppeteer/core/pageClick.ts
@@ -4,53 +4,41 @@ import { state } from "../state";
 export const pageClick = async (data: IPageClick) => {
         const { instance, selector, options, iframe } = data;
         try {
+								var page = state[instance]?.page
+
                 if (iframe) {
-                        const frameElement = await state[instance]?.page.$(iframe);
-                        if (!frameElement) {
-                                return {
-                                        error: "Error to find the iframe element"
-                                }
-                        }
-                        const frame = await frameElement.contentFrame();
-                        if (!frame) {
-                                return {
-                                        error: "Error to find the iframe"
-                                }
-                        }
+										const frameElement = await page.$(iframe);
+										if (!frameElement) {
+												return {
+														error: "Error to find the iframe element"
+												}
+										}
+										const frame = await frameElement.contentFrame();
+										if (!frame) {
+												return {
+														error: "Error to find the iframe"
+												}
+										}
+										page = frame
+								}
 
-                        const [response] = await Promise.all([
-                                frame.waitForNavigation(options),
-                                frame.click(selector, options),
-                        ]);
-                        if (response?.error) {
-                                return {
-                                        error: response?.error || "Error to click in the selector"
-                                }
-                        }
-                        return {
-                                status: "success",
-                                message: "Clicked in the selector"
-                        }
-
-                } else {
-                        const [response] = await Promise.all([
-                                state[instance]?.page.waitForNavigation(options),
-                                state[instance]?.page.click(selector, options),
-                        ]);
-                        if (response?.error) {
-                                return {
-                                        error: response?.error || "Error to click in the selector"
-                                }
-                        }
-                        return {
-                                status: "success",
-                                message: "Clicked in the selector"
-                        }
-                }
+								const [response] = await Promise.all([
+										page.waitForNavigation(options),
+										page.click(selector, options)
+								]);
+								if (response?.error) {
+										return {
+													error: response?.error || "Error to click in the selector"
+										}
+								}
+								return {
+										status: "success",
+										message: "Clicked in the selector"
+								}
 
         } catch (error: any) {
                 return {
-                        error: error?.message || "Error to click in the selector"
+                    error: error?.message || "Error to click in the selector"
                 }
         }
 }
--- nodes/Puppeteer/puppeteer/core/pageHover.ts
+++ nodes/Puppeteer/puppeteer/core/pageHover.ts
@@ -4,66 +4,51 @@ import { state } from "../state";
 export const pageHover = async (data: IPageHover) => {
         const { instance, selector, iframe } = data;
         try {
-                if (iframe) {
-                        const frameElement = await state[instance]?.page.$(iframe);
-                        if (!frameElement) {
-                                return {
-                                        error: "Error to find the iframe element"
-                                }
-                        }
-                        const frame = await frameElement.contentFrame();
-                        if (!frame) {
-                                return {
-                                        error: "Error to find the iframe"
-                                }
-                        }
-
-                        const [response] = await Promise.all([
-                                frame.waitForNavigation(),
-                                frame.hover(selector),
-                        ]);
-                        if (response?.error) {
-                                return {
-                                        error: response?.error || "Error to hover in the selector"
-                                }
-                        }
-                        return {
-                                status: "success",
-                                message: "Hover in the selector"
-                        }
-
-                } else {
-
-										try {	//Manual hover that works with Shadow DOM
-											const el = await state[instance]?.page.$(selector)
-											const boundingBox = await el.boundingBox()
-											const x = boundingBox.x + boundingBox.width/2
-											const y = boundingBox.y + boundingBox.height/2
-											await state[instance]?.page.mouse.move(x, y)
-
+								var page = state[instance]?.page
+								if (iframe) {
+										const frameElement = await page.$(iframe);
+										if (!frameElement) {
+												return {
+														error: "Error to find the iframe element"
+												}
 										}
-										catch {
-											return {
-												error: "Error when hovering on selector"
-											}
+										const frame = await frameElement.contentFrame();
+										if (!frame) {
+												return {
+														error: "Error to find the iframe"
+												}
 										}
-										//detect hover here (How?)
+										page = frame
+								}
 
-                        /*const [response] = await Promise.all([
-                                //state[instance]?.page.waitForNavigation(),
-                                //state[instance]?.page.hover(selector),
+								try {	//Manual hover that works with Shadow DOM
+										const el = await page.$(selector)
+										const boundingBox = await el.boundingBox()
+										const x = boundingBox.x + boundingBox.width/2
+										const y = boundingBox.y + boundingBox.height/2
+										await page.mouse.move(x, y)
+								}
+								catch {
+										return {
+											error: "Error when hovering on selector"
+										}
+								}
+								//detect hover here (How?)
 
-                        ]);*/
-                        /*if (response?.error) {
-                                return {
-                                        error: response?.error || "Error to hover in the selector"
-                                }
-                        }*/
-                        return {
-                                status: "success",
-                                message: "Hover in the selector"
-                        }
-                }
+										/*const [response] = await Promise.all([
+														//state[instance]?.page.waitForNavigation(),
+														//state[instance]?.page.hover(selector),
+
+										]);*/
+										/*if (response?.error) {
+														return {
+																		error: response?.error || "Error to hover in the selector"
+														}
+										}*/
+									return {
+										status: "success",
+										message: "Hover in the selector"
+									}
 
         } catch (error: any) {
                 return {
